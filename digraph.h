f90003631fbd6ec110e7d4fb691c5e55c35873b5
JoshuaMusch <muschja@mail.uc.edu>
2020-04-03 16:23:30 -0400

    Adding functionality to all Digraph functions

--- digraph.h
+++ digraph.h
@@ -1,24 +1,34 @@
 
 #include <vector>
+#include <map>
 
 #include "node.h"
 
 // Class for digraph
 class Digraph {
 private:
-    vector<Node*> m_headers;
+    std::vector<Node*> m_headers;
+    bool          m_hasCycle;
 
 public:
     Digraph           ();
 
-    // v1 is the vertex the edge starts at and v2 is the vertex it goes to.
+    // v1 is the vertex the edge starts at & v2 is the vertex it goes to.
 
+    // Utility Functions  - - - - - - - //
+    bool  CheckBounds (int v1, int v2   );
+    bool  SortUtil    (std::map<std::string, int>*  overallVisits,
+                       std::map<std::string, bool>* cycleVisits,
+                       Node**                       head,
+                       Node*                        v1
+                      );
+
+    // Main Functions - - - - - - - - - //
     bool  AddEdge     (int v1, int v2   );
-    bool  AddVertex   (std::string value);
     bool  HasEdge     (int v1, int v2   );
     bool  RemoveEdge  (int v1, int v2   );
-    bool  RemoveVertex(std::string value);
-    Node* Sort        ();
+    bool  AddVertex   (std::string value);
+    Node* Sort        (                 );
 
     ~Digraph          ();
-}
+};
