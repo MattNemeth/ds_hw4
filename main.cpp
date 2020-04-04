daa9af5410d825f669a04fe1b8ad0359548485b2
mattnemeth <nemeth.matthew@outlook.com>
2020-04-04 19:12:58 -0400

    added basic stuff to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@
 
 int main() {
 
+    std::cout << "Beginning of Assignment 4 program." << std::endl;
+
+    std::string inputs[50];
+    int i = 0;
+
+    while(usrinputting == True){
+        std::cout << "Input the task: " << std::endl;
+        std::cin >> inputs[i];
+        std::cout << "The value you entered is: " << inputs[i] << std::endl;
+        // std::cout << "Is this correct?" << std::endl;
+        i++; //increment counter
+
+    }
+
     return 0;
 }
