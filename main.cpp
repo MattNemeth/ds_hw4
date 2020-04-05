334055daff0fb2eb8d92c0f68118ca737445f6a5
MattNemeth <nemeth.matthew@outlook.com>
2020-04-05 17:31:37 -0400

    pushing most recent changes. Still working on user input portion of main.cpp

--- main.cpp
+++ main.cpp
@@ -11,24 +11,54 @@ int main() {
     std::cout << "Enter 1 when you have finished entering in all tasks." << std::endl;
 
     std::string inputs[50]; //array of strings. max of 50 tasks
+    vector<std::string> tasks;
     std::string task;
-    int i = 0;
+    int taskCount = 0;
 
     while(1){
-        std::cout << "Input a string for task " << i << ": ";
-        getline(cin, task);
-	inputs[i] = task;
-        std::cout << "You entered is: " << task << std::endl;
+        std::cout << "Input a string for task " << taskCount << ": ";
+        getline(std::cin, task);
+	tasks[taskCount] = task;
+        // std::cout << "You entered: " << task << std::endl;
 
 
-	if (task == '1'){
+	if (task == "1"){
 		break;
 	}
 
 
 
 
-        i++; //increment counter
+        taskCount++; //increment counter
+	//add the vertex here
+
+    }
+
+
+    std::cout << "Task entering is completed.\n" << std::endl;
+    std::cout << "Task list: " << std::endl;
+    for(int i=0; i < taskCount; i++){
+	    std::cout << i << ".) " << tasks[i] << std::endl;
+    } 
+    std::cout << std::endl;
+
+    std::cout << "To add the edges, enter which task needs to be completed first.\n";
+    std::cout << "Use the number to the left of each task above.\n";
+    std::cout << "For example:" << std::endl << "3 1 indicates task 3 must precede task 1" << std::endl;
+    std::cout << "7 5 indicates task 7 must precede task 5" << std::endl;
+    std::cout << "Enter 1 to stop." << std::endl;
+
+    std::string input = "";
+
+    while(1){
+	    std::cout << "Enter which task comes first: " << std::endl;
+	    getline(std::cin, input); 
+
+	    if(input == "1"){
+		    break;
+	    }
+
+	    
 
     }
 
