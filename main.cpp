a98e48e05906ebe84acc0dae12acd692f8c1b493
Matt Nemeth <nemeth.matthew@outlook.com>
2020-04-05 15:49:42 -0400

    can now input strings for tasks within main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 // main source code file
 
 #include "digraph.h"
+#include <string>
+#include "iostream"
+
 
 int main() {
 
     std::cout << "Beginning of Assignment 4 program." << std::endl;
+    std::cout << "Enter 1 when you have finished entering in all tasks." << std::endl;
 
-    std::string inputs[50];
+    std::string inputs[50]; //array of strings. max of 50 tasks
+    std::string task;
     int i = 0;
 
-    while(usrinputting == True){
-        std::cout << "Input the task: " << std::endl;
-        std::cin >> inputs[i];
-        std::cout << "The value you entered is: " << inputs[i] << std::endl;
-        // std::cout << "Is this correct?" << std::endl;
+    while(1){
+        std::cout << "Input a string for task " << i << ": ";
+        getline(cin, task);
+	inputs[i] = task;
+        std::cout << "You entered is: " << task << std::endl;
+
+
+	if (task == '1'){
+		break;
+	}
+
+
+
+
         i++; //increment counter
 
     }
