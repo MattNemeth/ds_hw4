40297d44345b8fd443e0333da869f1c87d15bd43
MattNemeth <nemeth.matthew@outlook.com>
2020-04-07 17:09:01 -0400

    Implemented num num for adding edges

--- main.cpp
+++ main.cpp
@@ -49,21 +49,49 @@ int main() {
 
     std::cout << "To add the edges, enter which task needs to be completed first.\n";
     std::cout << "Use the number to the left of each task above.\n";
-    std::cout << "For example:" << std::endl << "3 1 indicates task 3 must precede task 1" << std::endl;
+    std::cout << "For example:\n3 1 indicates task 3 must precede task 1" << std::endl;
     std::cout << "7 5 indicates task 7 must precede task 5" << std::endl;
     std::cout << "Enter 1 to stop." << std::endl;
 
     std::string input = "";
 
     while(1){
-	    std::cout << "Enter which task comes first: " << std::endl;
+	    std::cout << "Enter based on which task comes first: " << std::endl;
 	    getline(std::cin, input); 
 
 	    if(input == "1"){
 		    break;
 	    }
 
-	    
+	   //implement here 
+	    std::vector<std::string> twoNum;
+	    std::string oneChar = "";
+
+	    for(int i=0; i<input.length(); i++){
+		if(input[i] != ' '){
+		   oneChar = input[i]; 
+		    twoNum.push_back(oneChar); 
+		}
+	    }
+
+	    if(twoNum.size() != 2){
+		std::cout << "Error: More than 2 characters were given." << std::endl;
+	    }
+
+
+
+		// Convert to integers
+		int from = std::stoi(twoNum[0]) - 1;
+		int to   = std::stoi(twoNum[1]) - 1;
+
+		// Try and add the edge. If it returns false, we had a problem
+		if (!DAG->AddEdge(from, to)) {
+			std::cout << "Invalid. Try again." << std::endl;
+			continue;
+		}
+
+		
+
 
     }
 
@@ -78,9 +106,11 @@ int main() {
     else {
 	std::cout << "Topological order of tasks: " << std::endl;
 	while(current != nullptr){
-		std::cout << current->GetValue() <<std::endl;
+		std::cout << ".) ";
+		std::cout << current->GetValue() << std::endl;
 		current = current->GetNext();
-	    }
+	}
+	std::cout << "End ***" << std::endl;
     }
 
     return 0;
